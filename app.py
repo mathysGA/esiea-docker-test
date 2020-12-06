f9a8061848760b554dcacbb1446d56ee420f0cf3
mathysGA <gageot@et.esiea.fr>
2020-12-06 16:49:34 +0100

    first init

---
+++ app.py
@@ -0,0 +1,13 @@
+# app.py
+import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+        return "Welcome"
+@app.route('/how are you')
+def hello():
+        return 'I am goof, how about yoiu ?'
+if __name__=="__main__":
+        app.run()
